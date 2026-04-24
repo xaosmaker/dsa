1c3103355024fb10505320d4e6f29dc46da040a2
xaosmaker <drosinakis.drosos1@gmail.com>
2026-04-25 00:41:46 +0300

    feat: continue learning

---
+++ implement.md
@@ -0,0 +1,18 @@
+Array
+
+dynamic Array 
+
+push 
+pop
+display
+insert -> index
+delete -> index
+delete -> item
+linear Search
+binary Search
+get
+Set
+Avg
+Max
+
+
--- recursion/main.c
+++ recursion/main.c
@@ -1,7 +1,43 @@
 
 
+int sum(int n) {
+
+  if (n <= 0) {
+    return 0;
+  }
+
+  return sum(n - 1) + n;
+}
+
+int factorial(int n) {
+
+  if (n <= 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+int ex(int b, int e) {
+  if (e == 0) {
+    return 1;
+  }
+
+  if (e % 2 == 0) {
+    return ex(b * b, e / 2);
+  }
+
+  return ex(b, e - 1) * b;
+}
+
 #include <stdio.h>
 int main(void) {
   printf("Hello from rec\n");
+  int n = 5;
+
+  printf("sum(%d) = %d\n", n, sum(n));
+  printf("form n*(n+1) /2 = %d\n", n * (n + 1) / 2);
+  printf("factorial(%d) = %d\n", n, factorial(n));
+  printf("ex(%d) = %d\n", n, ex(2, 16));
   return 0;
 }
